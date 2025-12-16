29dc89273743efa6812f54a972ee71791838b4a5
Brayan <bittolo.8bit@gmail.com>
2025-12-16 20:50:10 +0100

    rearranging code and fixing validations

--- js/main.js
+++ js/main.js
@@ -1,48 +1,53 @@
-let userName = NaN;
+let userName = "";
+let age = NaN;
+let pricePerKm = NaN;
+let km = NaN;
 
-while (!isNaN(parseInt(userName))) {
-  userName = prompt("Inserisci il tuo nome:");
+// name input validation
+while (!isNaN(parseInt(userName)) || userName.length === 0) {
+  userName = prompt("Inserisci il tuo nome:").toLowerCase();
 }
-userName.toLowerCase();
 console.log(userName);
 
-let km = NaN;
-while (isNaN(km)) {
-  km = parseInt(prompt("Quanti chilometri devi percorrere?"));
-}
-console.log(km);
-
-let age = NaN;
-while (isNaN(age)) {
+// age input validation
+while (isNaN(age) || age === 0) {
   age = parseInt(prompt("Quanti anni hai?"));
 }
 console.log(age);
 
-let pricePerKm = NaN;
-if (userName == "tiziano") {
-  pricePerKm = 0.5;
+// km input validation
+while (isNaN(km) || km === 0) {
+  km = parseInt(prompt("Quanti chilometri devi percorrere?"));
+}
+console.log(km);
+
+// price per km tweaking
+if (userName === "tiziano") {
+  pricePerKm = 0.5; // pricePerKm penalty for blacklisted users
 } else {
-  pricePerKm = 0.21;
+  pricePerKm = 0.21; // pricePerKm standard users
 }
 console.log(`prezzo per km = ${pricePerKm}€`);
 
+// ticket price calculation
 let ticketPrice = km * pricePerKm;
 console.log(`standard price = ${ticketPrice}€`);
 
-if (userName == "artur") {
+// apply discount if needed
+if (userName === "artur") {
   ticketPrice = 0;
   console.log("Questo giro lo offre la casa!");
 } else if (age <= 18) {
   ticketPrice *= 0.8;
-  console.log("<=18 discount");
+  console.log("under 18 discount");
 } else if (age >= 65) {
   ticketPrice *= 0.6;
-  console.log(">=65 discount");
+  console.log("over 65 discount");
 }
 
 console.log(ticketPrice.toFixed(2));
-if (ticketPrice == 0) {
-  alert(`Per te il biglietto è gratuito`);
+if (ticketPrice === 0) {
+  alert(`Questo giro lo offre la casa!`);
 } else {
   alert(`Il costo del biglietto è ${ticketPrice.toFixed(2)} €`);
 }
