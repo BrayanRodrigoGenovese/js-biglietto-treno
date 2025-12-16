cfb9f9e04a24ff19e13ab5f46aab6d6660f4043c
Brayan <bittolo.8bit@gmail.com>
2025-12-16 13:39:36 +0100

    adding log messages

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 const km = prompt("Quanti chilometri devi percorrere?");
+console.log(km);
 const age = prompt("Quanti anni hai?");
+console.log(age);
 const price = 0.21;
+console.log(price);
 
 let ticketPrice = km * price;
+console.log(`standard price = ${price}€`);
 if (age <= 18) {
   ticketPrice *= 0.8;
+  console.log("<=18 discount");
 } else if (age >= 65) {
   ticketPrice *= 0.6;
+  console.log(">=65 discount");
 }
 
 console.log(ticketPrice);
